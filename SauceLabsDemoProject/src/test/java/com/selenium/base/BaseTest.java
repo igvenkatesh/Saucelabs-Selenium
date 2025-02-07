de83fc0210c5ea79d6d0fd4f1dd04d7579323bf0
venkatesh <venkateshgiyengar@gmail.com>
2025-02-07 14:25:08 +0530

    added extent reporting

--- SauceLabsDemoProject/src/test/java/com/selenium/base/BaseTest.java
+++ SauceLabsDemoProject/src/test/java/com/selenium/base/BaseTest.java
@@ -1,31 +1,33 @@
 package com.selenium.base;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Listeners;
 
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-
+import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
+//@Listeners({ExtentITestListenerClassAdapter.class})
 public class BaseTest {
 
-    protected WebDriver driver;
+	protected WebDriver driver;
 
-    @BeforeSuite
-    public void setUp() {
-       
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();
-        driver.get("https://www.saucedemo.com/v1/index.html");
-    }
+	@BeforeSuite
+	public void setUp() {
 
-    @AfterSuite
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		driver.get("https://www.saucedemo.com/v1/index.html");
+	}
+
+	@AfterSuite
+	public void tearDown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
 }
 
